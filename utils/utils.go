a889a8daed6840e878623860484e58d9452dc79f
Dmitry Vorobev <dimahabr@gmail.com>
2015-10-25 20:09:05 +0100

    Add utils package and UUID function.

---
+++ utils/utils.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var Random *os.File
+
+func init() {
+	f, err := os.Open("/dev/urandom")
+	if err != nil {
+		log.Fatal(err)
+	}
+	Random = f
+}
+
+func UUID() string {
+	b := make([]byte, 16)
+	Random.Read(b)
+	return fmt.Sprintf("%x-%x-%x-%x-%x",
+		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+}
