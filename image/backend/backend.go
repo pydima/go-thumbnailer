effead2821270b05bf4a7586abdbc328b0b5939e
Dmitry Vorobev <dimitriy.vorobyev@fyndiq.com>
2015-12-08 11:54:58 +0100

    Fix typo.

--- image/backend/backend.go
+++ image/backend/backend.go
@@ -1,6 +1,6 @@
 package backend
 
-type ImageBaсkander interface {
+type ImageBackender interface {
 	Exists(hash string) bool
 	Save(img []byte, hash string) (path string, err error)
 	Load(hash string) (img []byte)
