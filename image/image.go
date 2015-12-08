0dd55638426f55e28eeb0a8c51eb6966a15fe2d6
Dmitry Vorobev <dimitriy.vorobyev@fyndiq.com>
2015-12-08 11:39:52 +0100

    Add image type detection.

--- image/image.go
+++ image/image.go
@@ -2,6 +2,7 @@ package image
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -13,17 +14,32 @@ import (
 	"github.com/pydima/go-thumbnailer/config"
 )
 
+var (
+	MARKER_JPG = []byte{0xff, 0xd8}
+	MARKER_PNG = []byte{0x89, 0x50}
+	MARKER_GIF = []byte{0x47, 0x49}
+)
+
+type ImageType int
+
+const (
+	UNKNOWN ImageType = iota
+	JPG
+	PNG
+	GIF
+)
+
 type Image struct {
 	width  uint
 	height uint
 	path   string
 }
 
-type InvalidExtension struct {
+type InvalidImage struct {
 	err string
 }
 
-func (e InvalidExtension) Error() string {
+func (e InvalidImage) Error() string {
 	return e.err
 }
 
@@ -33,14 +49,31 @@ func checkExtension(n string) error {
 			return nil
 		}
 	}
-	return InvalidExtension{fmt.Sprintf("Cannot handle %s extension")}
+	return InvalidImage{fmt.Sprintf("Cannot handle %s extension")}
+}
+
+func checkImageFormat(b []byte) ImageType {
+	if len(b) < 2 {
+		return UNKNOWN
+	}
+
+	switch {
+	case bytes.Equal(b[:2], MARKER_JPG):
+		return JPG
+	case bytes.Equal(b[:2], MARKER_PNG):
+		return PNG
+	case bytes.Equal(b[:2], MARKER_GIF):
+		return GIF
+	default:
+		return UNKNOWN
+	}
 }
 
 func ProcessImage(i io.ReadCloser) (path string, err error) {
 	options := vips.Options{
 		Width:        100,
 		Height:       100,
-		Crop:         false,
+		Crop:         true,
 		Extend:       vips.EXTEND_WHITE,
 		Interpolator: vips.BILINEAR,
 		Gravity:      vips.CENTRE,
--- image/image_test.go
+++ image/image_test.go
@@ -1,6 +1,8 @@
 package image
 
 import (
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/pydima/go-thumbnailer/config"
@@ -21,5 +23,42 @@ func TestCheckExtension(t *testing.T) {
 	err = checkExtension("file.JPG")
 	if err != nil {
 		t.Errorf("Cannot detect extension in uppercase.")
+	}
+}
+
+func TestCheckImageFormat(t *testing.T) {
+	bp := "/go/src/github.com/pydima/go-thumbnailer/testdata/"
+
+	readAndCheckFile := func(name string) []byte {
+		b, err := ioutil.ReadFile(filepath.Join(bp, name))
+		if err != nil {
+			t.Errorf("Cannot read the test file")
+		}
+		return b
+	}
+
+	b := readAndCheckFile("jpg.jpg")
+	if f := checkImageFormat(b); f != JPG {
+		t.Errorf("Invalid detection of jpg file.")
+	}
+
+	b = readAndCheckFile("png.png")
+	if f := checkImageFormat(b); f != PNG {
+		t.Errorf("Invalid detection of png file.")
+	}
+
+	b = readAndCheckFile("gif.gif")
+	if f := checkImageFormat(b); f != GIF {
+		t.Errorf("Invalid detection of gif file.")
+	}
+
+	b = readAndCheckFile("bmp.bmp")
+	if f := checkImageFormat(b); f != UNKNOWN {
+		t.Errorf("Invalid detection of unknown file format.")
+	}
+
+	var bt []byte
+	if f := checkImageFormat(bt); f != UNKNOWN {
+		t.Errorf("Invalid detection of unknown file format.")
 	}
 }
