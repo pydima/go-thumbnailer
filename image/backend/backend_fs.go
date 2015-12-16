281b1bd3523fad5bcfddcc33665b35b4850f013c
Dmitry Vorobev <dimitriy.vorobyev@fyndiq.com>
2015-12-16 09:19:26 +0100

    Refactoring.

--- image/backend/backend_fs.go
+++ image/backend/backend_fs.go
@@ -35,47 +35,86 @@ func (fb FSBackend) generateDest(imgs map[string][]byte, basePath string) []stri
 	return res
 }
 
-func (fb FSBackend) Save(imgs map[string][]byte) ([]string, error) {
-	var (
-		found bool
-		img   []byte
-	)
+func (fb FSBackend) createTmpDir() (tmpDir string, err error) {
+	baseTempDir := filepath.Join(fb.BasePath, "tmp")
+	tmpDir, err = ioutil.TempDir(baseTempDir, time.Now().Format(time.RFC3339)+"_")
+
+	if err != nil {
+		pathErr, ok := err.(*os.PathError)
+		if !ok {
+			return
+		}
+
+		if pathErr.Err != syscall.ENOENT {
+			return
+		} else {
+
+			if err = os.MkdirAll(baseTempDir, 0755); err != nil {
+				return
+			}
+
+			tmpDir, err = ioutil.TempDir(baseTempDir, time.Now().Format(time.RFC3339)+"_")
+			if err != nil {
+				return
+			}
+		}
+	}
+	return tmpDir, nil
+}
+
+func (fb FSBackend) baseImage(imgs map[string][]byte) ([]byte, error) {
+	var img []byte
+
 	for k, v := range imgs {
 		if strings.HasPrefix(k, "original") { // image might have different extensions e.g. 'jpg', 'png'
 			img = v
-			found = true
 			break
 		}
 	}
-	if !found {
+
+	if img == nil {
 		return nil, fmt.Errorf("cannot find original image.")
+	}
+
+	return img, nil
+}
+
+func (fb FSBackend) moveFiles(src, dst string) (err error) {
+	parentDir := filepath.Dir(dst)
+	err = os.MkdirAll(parentDir, 0755)
+	if err != nil {
+		return
+	}
+
+	if err = os.Rename(src, dst); err != nil {
+		linkErr, ok := err.(*os.LinkError)
+		if !ok {
+			return
+		}
+		if linkErr.Err != syscall.ENOTEMPTY {
+			return
+		}
+	}
+	return
+}
+
+func (fb FSBackend) Save(imgs map[string][]byte) ([]string, error) {
+	img, err := fb.baseImage(imgs)
+	if err != nil {
+		return nil, err
 	}
 
 	s := sha1.Sum(img)
 	hash := base64.URLEncoding.EncodeToString(s[:])
+
 	dir := fb.generatePath(hash)
 	if fb.exists(dir) {
 		return fb.generateDest(imgs, dir), nil
 	}
 
-	baseTempDir := filepath.Join(fb.BasePath, "tmp")
-	tmpDir, err := ioutil.TempDir(baseTempDir, time.Now().Format(time.RFC3339)+"_")
+	tmpDir, err := fb.createTmpDir()
 	if err != nil {
-		pathErr, ok := err.(*os.PathError)
-		if !ok {
-			return nil, err
-		}
-		if pathErr.Err != syscall.ENOENT {
-			return nil, err
-		} else {
-			if err := os.MkdirAll(baseTempDir, 0755); err != nil {
-				return nil, err
-			}
-			tmpDir, err = ioutil.TempDir(baseTempDir, time.Now().Format(time.RFC3339)+"_")
-			if err != nil {
-				return nil, err
-			}
-		}
+		return nil, err
 	}
 
 	defer func() {
@@ -90,20 +129,8 @@ func (fb FSBackend) Save(imgs map[string][]byte) ([]string, error) {
 		}
 	}
 
-	parentDir := filepath.Dir(dir)
-	err = os.MkdirAll(parentDir, 0755)
-	if err != nil {
+	if err = fb.moveFiles(tmpDir, dir); err != nil {
 		return nil, err
-	}
-
-	if err := os.Rename(tmpDir, dir); err != nil {
-		linkErr, ok := err.(*os.LinkError)
-		if !ok {
-			return nil, err
-		}
-		if linkErr.Err != syscall.ENOTEMPTY {
-			return nil, err
-		}
 	}
 
 	return fb.generateDest(imgs, dir), nil
