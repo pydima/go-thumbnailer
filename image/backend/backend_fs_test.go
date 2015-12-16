5bdcfa90f142fbe76df29ff5a5819da5fa2ceb7e
Dmitry Vorobev <dimitriy.vorobyev@fyndiq.com>
2015-12-16 10:54:19 +0100

    Add tests.

--- image/backend/backend_fs_test.go
+++ image/backend/backend_fs_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -71,5 +72,99 @@ func TestGenerateDest(t *testing.T) {
 	}
 	if res[0] != want[0] {
 		t.Errorf("Got %s, wanted: %s", res, want)
+	}
+}
+
+func TestCreateTmpDir(t *testing.T) {
+	defer cleanUp()
+	baseTempDir := filepath.Join(backend.BasePath, "tmp")
+	if _, err := os.Stat(baseTempDir); !os.IsNotExist(err) {
+		t.Errorf("Base temp directory already exists.")
+	}
+
+	// create directory when parent directory doesn't exist
+	tmpDir, err := backend.createTmpDir()
+	if err != nil {
+		t.Errorf("Couldn't create directory.")
+	}
+	os.Remove(tmpDir)
+
+	// create directory when parent directory exists
+	tmpDir, err = backend.createTmpDir()
+	if err != nil {
+		t.Errorf("Couldn't create directory.")
+	}
+}
+
+// Should always pass image with name 'original.*' in images map.
+func TestBaseImage(t *testing.T) {
+	// test with 'original.png'
+	imgs := map[string][]byte{
+		"original.png": []byte("image"),
+	}
+	_, err := backend.baseImage(imgs)
+	if err != nil {
+		t.Errorf("Got error %s", err)
+	}
+
+	// test with 'original.jpg'
+	imgs = map[string][]byte{
+		"original.jpg": []byte("image"),
+	}
+	_, err = backend.baseImage(imgs)
+	if err != nil {
+		t.Errorf("Got error %s", err)
+	}
+
+	// without 'original.*' should get an error
+	imgs = map[string][]byte{
+		"127x127.png": []byte("image"),
+	}
+	_, err = backend.baseImage(imgs)
+	if err == nil {
+		t.Errorf("Should get an error, didn't pass original image.")
+	}
+}
+
+func TestMoveFiles(t *testing.T) {
+	defer cleanUp()
+
+	dst := filepath.Join(backend.BasePath, "new_dir")
+	tmpDir, err := backend.createTmpDir()
+	if err != nil {
+		t.Errorf("Couldn't create directory.")
+	}
+
+	// move directory when dst doesn't exist
+	err = backend.moveFiles(tmpDir, dst)
+	if err != nil {
+		t.Errorf("Got error %s", err)
+	}
+
+	// move directory when dst exists and empty
+	tmpDir, err = backend.createTmpDir()
+	if err != nil {
+		t.Errorf("Couldn't create directory.")
+	}
+	err = backend.moveFiles(tmpDir, dst)
+	if err != nil {
+		t.Errorf("Got error %s", err)
+	}
+
+	// move directory when dst exists and not empty
+	tmpDir, err = backend.createTmpDir()
+	if err != nil {
+		t.Errorf("Couldn't create directory.")
+	}
+
+	f, err := os.Create(filepath.Join(dst, "file"))
+	if err != nil {
+		t.Errorf("Couldn't create file %s.", err)
+	}
+	f.Close()
+
+	err = backend.moveFiles(tmpDir, dst)
+	if err == nil {
+		t.Errorf("Should get an error if dst is not empty.")
 	}
 }
