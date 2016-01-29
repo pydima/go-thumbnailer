90e3400d010c82ab242f49bb3a3a5f8544d17706
Dmitry Vorobev <dimitriy.vorobyev@fyndiq.com>
2016-01-29 16:39:18 +0100

    Add test for Notify function.

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"syscall"
 	"testing"
@@ -27,4 +31,54 @@ func TestHandleSigTerm(t *testing.T) {
 
 	// process shouldn't panic if get signal twice
 	syscall.Kill(os.Getpid(), syscall.SIGTERM)
+}
+
+func TestNotify(t *testing.T) {
+	urls := make([]string, 0)
+	urls = append(urls, "one")
+	urls = append(urls, "two")
+	idExpected := "id_string"
+
+	var (
+		idResult   string
+		urlsResult []string
+	)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+
+		ack := Ack{}
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Errorf("Got an error %s", err)
+		}
+
+		json.Unmarshal(data, &ack)
+		idResult = ack.ID
+		urlsResult = ack.Images
+	}))
+	defer ts.Close()
+
+	ack := NewAck(ts.URL, idExpected, urls)
+
+	if err := Notify(ack); err != nil {
+		t.Errorf("got an error %s", err)
+	}
+
+	if idResult != idExpected {
+		t.Errorf("wrong result, got: %s - expected: %s", idResult, idExpected)
+		return
+	}
+
+	if len(urls) != len(urlsResult) {
+		t.Errorf("wrong result, got: %s - expected: %s", urls, urlsResult)
+		return
+	}
+
+	for i, _ := range urls {
+		if urls[i] != urlsResult[i] {
+			t.Errorf("wrong result, got: %s - expected: %s", urls, urlsResult)
+			break
+		}
+	}
 }
